e2e042a806a42f24029e1ccdc2f1fa37932b0b64
Aria143 <129579777+Aria143@users.noreply.github.com>
2023-04-02 17:43:49 +0530

    Update your_file_name.py

--- your_file_name.py
+++ your_file_name.py
@@ -35,6 +35,6 @@ dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_me
 
 # Start the bot
 port = int(os.environ.get("PORT",443))
-updater.start_webhook(listen="0.0.0.0", port=port, url_path=bot_token)
+updater.start_webhook(listen="80", port=port, url_path=bot_token)
 updater.bot.setWebhook(os.getenv("APP_URL") + bot_token)
 updater.idle()
